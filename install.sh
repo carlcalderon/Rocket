4d45298bfc7ed163f36cc5b088f5e938b4dca6f7
Carl Calderon <carl.calderon@gmail.com>
2013-01-23 01:16:49 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,1 @@
-sudo git clone -b "deploy" https://github.com/carlcalderon/Rocket.git /usr/local/lib/rocket
-sudo ln -s /usr/local/lib/rocket/rocket /usr/local/bin/rocket
+sudo git clone -b "deploy" https://github.com/carlcalderon/Rocket.git /usr/local/lib/rocket; sudo ln -s /usr/local/lib/rocket/rocket /usr/local/bin/rocket
