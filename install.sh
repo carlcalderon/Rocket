2a2d4fc612a287e2c5d7b44572876b04348d1ae9
Carl Calderon <carl.calderon@gmail.com>
2013-01-23 01:16:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,2 @@
-# install.sh
-
-# download Rocket
-sudo git clone -b "deploy" https://github.com/carlcalderon/Rocket.git /usr/local/lib/rocket;
-
-# create global symlink
+sudo git clone -b "deploy" https://github.com/carlcalderon/Rocket.git /usr/local/lib/rocket
 sudo ln -s /usr/local/lib/rocket/rocket /usr/local/bin/rocket
